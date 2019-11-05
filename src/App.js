3a98b91c87e8042e12ee822472d3ad4ce855cd47
Zbigniew Baranski <zb.baranski@icloud.com>
2019-11-05 01:31:58 +0100

    set Face API

--- src/App.js
+++ src/App.js
@@ -53,22 +53,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
-    function(response) {
-      console.log(response);
-    },
-    function(err) {
-      console.log(err);
-    }
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+        .then(
+          function(response) {
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+          },
+          function(err) {
+            console.log(err);
+          }
   );
   }
 
@@ -84,7 +89,7 @@ class App extends Component {
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
